a2c282660297a38160c3aca0c7d4bb83ac905817
Favian Medina <fabinnerself@gmail.com>
2025-07-08 12:45:21 -0400

    upd env var conection

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(express.json())
 
 // Conexión a MongoDB
 mongoose
-  //.connect(process.env.MONGODB_URI, {
-    .connect("mongodb+srv://favianmedinagemio:frxJ4XBUlplV9uJk@cluster0.0tyxhhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/tienda_db", {
+  .connect(process.env.MONGODB_URI, {    
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
